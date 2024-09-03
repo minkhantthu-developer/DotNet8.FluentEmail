e5fe1ee1a4f08118fe85a1ab82dcd36c1d7f021f
Min Khant Thu <dsalpha3367@gmail.com>
2024-09-03 21:59:31 +0630

    [Add] UserModel

---
+++ DotNet8.FluentEmail.WebApi/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿namespace DotNet8.FluentEmail.WebApi.Models
+{
+    public class UserModel
+    {
+        public string? UserName { get; set; }
+
+        public string? Email { get; set; }  
+
+        public string? MemberType { get; set; }
+
+        public UserModel(string userName, string email, string memberType)
+        {
+            UserName = userName;
+            Email = email;
+            MemberType = memberType;
+        }
+    }
+}
